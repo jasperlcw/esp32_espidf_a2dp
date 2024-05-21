2c221ac442ac5fbd72465815f92001632f7c4496
Jasper Wong <lazypigz.mw3@gmail.com>
2024-05-21 16:05:12 -0700

    updated definitions for metadata switch case

--- main/bt_app_metadata.c
+++ main/bt_app_metadata.c
@@ -1,22 +1,23 @@
 #include "bt_app_metadata.h"
+#include "esp_avrc_api.h"
 #include "esp_log.h"
 
 void bt_metadata_rc(uint8_t attr_id, uint8_t *attr_text)
 {
     switch(attr_id) {
-    case 0x1: {
-        ESP_LOGI(BT_METADATA_TAG, "Song: %s", attr_text);
+    case ESP_AVRC_MD_ATTR_TITLE: {
+        ESP_LOGI(BT_METADATA_TAG, "Title: %s", attr_text);
         break;
     }
-    case 0x2: {
+    case ESP_AVRC_MD_ATTR_ARTIST: {
         ESP_LOGI(BT_METADATA_TAG, "Artist: %s", attr_text);
         break;
     }
-    case 0x4: {
+    case ESP_AVRC_MD_ATTR_ALBUM: {
         ESP_LOGI(BT_METADATA_TAG, "Album: %s", attr_text);
         break;
     }
-    case 0x20: {
+    case ESP_AVRC_MD_ATTR_GENRE: {
         ESP_LOGI(BT_METADATA_TAG, "Genre: %s", attr_text);
         break;
     }
