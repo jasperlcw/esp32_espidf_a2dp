3764378e561eccfccb62c36b13bff7117a6418e4
Jasper Wong <lazypigz.mw3@gmail.com>
2024-05-23 13:53:42 -0700

    added call to send metadata over uart

--- main/bt_app_metadata.c
+++ main/bt_app_metadata.c
@@ -1,21 +1,43 @@
+#include <string.h>
 #include "bt_app_metadata.h"
+#include "bt_app_uart.h"
 #include "esp_avrc_api.h"
 #include "esp_log.h"
 
+unsigned char metadata_buf[512];
+
 void bt_metadata_rc(uint8_t attr_id, uint8_t *attr_text)
 {
+    bool send_flag = false;
     switch(attr_id) {
-        /* Fall through, send data over to another device over UART to process */
-    case ESP_AVRC_MD_ATTR_TITLE: {}
-    case ESP_AVRC_MD_ATTR_ARTIST: {}
-    case ESP_AVRC_MD_ATTR_ALBUM: {}
+    case ESP_AVRC_MD_ATTR_TITLE: {
+        ESP_LOGI(BT_METADATA_TAG, "Title: %s", attr_text);
+        send_flag = true;
+        break;
+    }
+    case ESP_AVRC_MD_ATTR_ARTIST: {
+        ESP_LOGI(BT_METADATA_TAG, "Artist: %s", attr_text);
+        send_flag = true;
+        break;
+    }
+    case ESP_AVRC_MD_ATTR_ALBUM: {
+        ESP_LOGI(BT_METADATA_TAG, "Album: %s", attr_text);
+        send_flag = true;
+        break;
+    }
     case ESP_AVRC_MD_ATTR_GENRE: {
-        // Send AVRC metadata over UART to a target device
+        ESP_LOGI(BT_METADATA_TAG, "Genre: %s", attr_text);
+        send_flag = true;
         break;
     }
     default: {
         ESP_LOGI(BT_METADATA_TAG, "Unhandled AVRC metadata: attribute id 0x%x, %s", attr_id, attr_text);
         break;
     }
-    } 
+    }
+
+    if (send_flag) {
+        sprintf(metadata_buf, "%x:%s", attr_id, attr_text);
+        bt_uart_async_send(metadata_buf, strlen(metadata_buf));
+    }
 }
